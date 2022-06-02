d6b4fabf30b99d93446a11379e8707d004fed1bf
nobers Veo <>
2022-06-02 20:50:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import discord
+from stockx import search
+
+token = "OTgxOTI4MDkwOTA3MzI4NTUz.GEqzVr.zEHn0Yk5dIg737o9RfOT27hCCoumIW2YDgA6mU"
+
+client = discord.Client()
+
+# channel_id = "981937387733151804" - Designated channel ID
+
+@client.event 
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # if message.channel.id != channel_id:
+    #     return
+
+    if message.content.split(' ')[0] == '!stockx':
+        query = message.content.replace('!stockx ', '')
+
+        item = search(query)
+
+        embedd = discord.Embed(
+            title = item['title'],
+            url = 'https://stockx.com/' + item['urlKey']
+        )
+        embedd.set_thumbnail(
+            url = item['media']['imageUrl']
+        )
+        embedd.add_field(
+            name= 'Colorway',
+            value=item['colorway']
+        )
+        embedd.add_field (
+            name = "Retail Price",
+            value = '$' + str(item['retailPrice'])
+        )
+        embedd.add_field (
+            name = 'Last Sale Price',
+            value = '$' + str(item['market']['lastSale'])
+        )
+        embedd.add_field(
+            name = 'Minimum Bid',
+            value = '$' + str(item['minimumBid'])
+        )
+        embedd.add_field(
+            name = 'Highest Bid',
+            value = '$' + str(item['market']['highestBid']),
+            inline = True
+        )
+        embedd.set_footer (
+            text = 'dev: nobers'
+        )
+        await message.channel.send(embed=embedd)
+
+    if message.content == '!stockxhelp':
+        embed = discord.Embed (
+            title = "**StockX Bot Help**"
+        )
+        embed.add_field(
+            name = "Commands",
+            value = "!stockx <name of shoe>"
+        )
+        embed.set_footer (
+            text = 'dev: nobers'
+        )
+        await message.channel.send(embed = embed)
+        
+client.run(token)
---
+++ stockx.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def search(query):
+    url = f'https://stockx.com/api/browse?_search={query}'
+
+    headers = {
+        'accept': 'application/json',
+        'accept-encoding': 'utf-8',
+        'accept-language': 'en-US,en;q=0.9',
+        'app-platform': 'Iron',
+        'referer': 'https://stockx.com/',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest'
+    }
+
+    html = requests.get(url=url, headers=headers)
+    output = json.loads(html.text)
+    return output['Products'][0]
